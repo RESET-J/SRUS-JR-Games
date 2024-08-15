a744a06d470a7a8595e8120ca6d8b8ade36468e6
unknown <joelresetar@gmail.com>
2024-08-15 13:12:49 +0800

    added / modified the player list class

--- app/player_list.py
+++ app/player_list.py
@@ -1,3 +1,5 @@
+from player import Player
+
 class PlayerList:
     def __init__(self, values=[]) -> None:
         self.head = None
---
+++ app/player_node.py
@@ -0,0 +1,31 @@
+from player import Player
+
+class PlayerNode: 
+    def __init__(self, player: Player) -> None:
+        self._player = player
+        self.key = self._player.uid
+        self._previous = None
+        self._succeeding = None
+
+    def __str__(self) -> str:
+        return f"previous {self._previous} this {self.key} succeeding {self._succeeding}"
+
+    @property
+    def previous(self):
+        return self._previous
+    
+    @previous.setter
+    def previous(self, value: Player):
+        self._previous = value
+
+    @property
+    def succeeding(self):
+        return self._succeeding
+    
+    @succeeding.setter
+    def succeeding(self, value):
+        self._succeeding = value
+
+    @property
+    def player(self):
+        return self._player
