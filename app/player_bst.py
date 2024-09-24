56f0e3521053ff14284af9b1cf8de3c99949c063
unknown <joelresetar@gmail.com>
2024-09-24 12:20:57 +0800

    Implemented player bst with insert, search and show functions and unittests for insert / search...

--- app/player_bst.py
+++ app/player_bst.py
@@ -1,3 +1,85 @@
+from player import Player
+from player_bnode import PlayerBNode
+
 class PlayerBST:
     def __init__(self) -> None:
         self._root = None
+
+    def insert(self, key: Player, current_node=None) -> None:
+        if current_node is None:
+            current_node = self._root
+
+        if self._root is None:
+            player = PlayerBNode(key)
+            self._root = player
+            return
+        if key.name == current_node.player.name:
+            current_node.player = key
+            return
+        if key.name < current_node.player.name:
+            if current_node.left is None:
+                current_node.left = PlayerBNode(key)
+            else:
+                self.insert(key=key, current_node=current_node.left)
+        else:
+            if current_node.right is None:
+                current_node.right = PlayerBNode(key)
+            else:
+                self.insert(key=key, current_node=current_node.right)
+
+    def search(self, name: str) -> Player:
+        return self._search(name=name, current_node=self._root)
+
+    # the assignment says to return none if the current node is equal to None however
+    # i felt that returning the player and an exception if the key was not found made more sense to me
+    # i am happy to change if preffered / required
+    def _search(self, name: str, current_node: PlayerBNode = None) -> Player:
+        if current_node is None:
+            raise KeyError("Error, the key is not found")
+        
+        if current_node.player.name == name:
+            return current_node.player
+        if current_node.player.name > name:
+            return self._search(name=name, current_node=current_node.left)
+        elif current_node.player.name < name:
+            return self._search(name=name, current_node=current_node.right)
+        
+    def _show(self, current_node: PlayerBNode = None, values: list = None) -> list:
+        """private function returns the items in the player bst as an list"""
+        if current_node is None:
+            return values
+        if values is None:
+            values = list()
+
+        return (self._show(
+            current_node.left, values) + [current_node.player] + self._show(current_node.right, values))
+    
+    def show(self) -> list:
+        """Shows the items in the player bst as an list"""
+        return self._show(current_node=self._root)
+
+
+        
+        
+
+
+if __name__ == "__main__":
+    bst = PlayerBST()
+
+    bst.insert(Player("01", "Player1"))
+    bst.insert(Player("02", "Player3"))
+    bst.insert(Player("00", "Player2"))
+    bst.insert(Player("03", "Player0"))
+    bst.insert(Player("04", "player3"))
+
+    print(bst._root.right.player.name)
+    print(bst._root.left)
+    print(bst._root.right.left.player)
+
+    # print(bst.search("Player"))
+
+    new_list = bst.show()
+
+    for item in new_list: 
+        print(item.name)
+        
---
+++ test/player_bst_test.py
@@ -0,0 +1,54 @@
+import unittest
+import sys
+sys.path.append('./app/')
+
+from app.player_bst import PlayerBST, Player
+import random
+
+class PlayerBSTTest(unittest.TestCase): 
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.bst = PlayerBST()
+
+    def test_player_bst_insert_value(self):
+        self.bst.insert(Player("01", "Player1"))
+        self.bst.insert(Player("03", "Player3"))
+        self.bst.insert(Player("02", "Player2"))
+        self.bst.insert(Player("05", "Player5"))
+        self.bst.insert(Player("04", "Player4"))
+
+        self.assertEqual(self.bst._root.player.name, "Player1")
+        self.assertEqual(self.bst._root.right.player.name, "Player3")
+        self.assertEqual(self.bst._root.right.right.left.player.name, "Player4")
+
+    def test_player_bst_insert_same_value(self):
+        self.bst.insert(Player("01", "Player1"))
+        self.bst.insert(Player("02", "Player1"))
+
+        self.assertEqual(self.bst._root.player.uid, "02")
+
+    def test_player_bst_search_when_empty(self):
+        with self.assertRaises(KeyError):
+            self.bst.search("Player1")
+
+    def test_player_bst_search(self):
+        self.bst.insert(Player("01", "Player1"))
+        self.bst.insert(Player("02", "Player2"))
+        self.bst.insert(Player("03", "Player3"))
+        
+        self.assertEqual(self.bst.search("Player1").name, "Player1")
+        self.assertEqual(self.bst.search("Player3").name, "Player3")
+
+    def test_player_bst_search_multiple(self):
+        values = random.sample(range(1000), 1000)
+
+        for value in values:
+            self.bst.insert(Player(str(value), "Player" + str(value)))
+
+        for i in range(100):
+            random_name = "Player" + str(random.choice(values))
+
+            self.assertEqual(self.bst.search(random_name).name, random_name)
+
+
